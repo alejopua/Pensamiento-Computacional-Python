88e20dfc929d372806ba55071d49d569e53c7e13
Pedro Ramirez <alejopua@gmail.com>
2022-12-20 14:12:46 -0500

    Iteraciones For v1.0

---
+++ iteracion_for.py
@@ -0,0 +1,35 @@
+# frutas = ['manzana', 'pera', 'mango']
+# for fruta in range(len(frutas)):
+#         print(fruta)
+
+# for cant in range(5):
+#         print(cant)
+
+# for cant in range(1,10):
+#         print(cant)
+
+for cant in range(1,10,2):
+        print(cant)
+
+# frutas = ['manzana', 'pera', 'mango']
+# for fruta in frutas:
+#         print(fruta)
+
+# estudiantes = {
+#     'mexico': 10,
+#     'colombia': 15,
+#     'puerto_rico': 4,
+# }
+
+# for pais in estudiantes:
+#     print(f'{pais}')
+
+# for pais in estudiantes.keys():
+#     print(f' -> {pais}')
+
+# for numero_de_estudiantes in estudiantes.values():
+#     print(f'-> {numero_de_estudiantes}')
+
+
+# for pais, numero_de_estudiantes in estudiantes.items():
+#     print(f'{pais} -> {numero_de_estudiantes}')
